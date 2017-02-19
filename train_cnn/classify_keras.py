503e3fc0871517c4ecca9867d0e3885d94fd5f18
bradyz <brady.zhou@utexas.edu>
2017-02-19 12:36:31 -0600

    training 10 epochs

--- train_cnn/classify_keras.py
+++ train_cnn/classify_keras.py
@@ -5,8 +5,8 @@ from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
 
 
-TRAIN_DIR = '/Users/bradyzhou/code/data/ModelNetViewpoints/train'
-VALID_DIR = '/Users/bradyzhou/code/data/ModelNetViewpoints/test'
+TRAIN_DIR = '/home/04365/bradyz/data/ModelNetViewpoints/train/'
+VALID_DIR = '/home/04365/bradyz/data/ModelNetViewpoints/test/'
 SAVE_FILE = 'model_weights.h5'
 LOG_FILE = 'training.log'
 NUM_CLASSES = 10
@@ -16,12 +16,14 @@ VALID = 5000
 
 
 def train(model):
-    train_datagen = ImageDataGenerator()
+    train_datagen = ImageDataGenerator(samplewise_center=True,
+            samplewise_std_normalization=True)
 
     train_generator = train_datagen.flow_from_directory(
             TRAIN_DIR, target_size=(IMAGE_SIZE, IMAGE_SIZE), batch_size=64)
 
-    valid_datagen = ImageDataGenerator()
+    valid_datagen = ImageDataGenerator(samplewise_center=True,
+            samplewise_std_normalization=True)
 
     valid_generator = valid_datagen.flow_from_directory(
             VALID_DIR, target_size=(IMAGE_SIZE, IMAGE_SIZE), batch_size=64)
@@ -56,8 +58,8 @@ def load_model_vgg():
 
     x = base_model.output
     x = Flatten(name='flatten')(x)
-    x = Dense(4096, activation='relu', name='fc1')(x)
-    x = Dense(2048, activation='relu', name='fc2')(x)
+    x = Dense(2048, activation='relu', name='fc1')(x)
+    x = Dense(1024, activation='relu', name='fc2')(x)
     x = Dense(NUM_CLASSES, activation='softmax', name='predictions')(x)
 
     model = Model(input=img_input, output=x)
