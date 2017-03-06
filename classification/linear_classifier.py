24803267a3ebd93bf0d361832f0995f5b5ae2f5c
bradyz <brady.zhou@utexas.edu>
2017-03-06 04:38:51 -0600

    svm classify by min k entropy

--- classification/linear_classifier.py
+++ classification/linear_classifier.py
@@ -15,8 +15,7 @@ from geometry_processing.utils.helpers import (get_data, samplewise_normalize,
 from geometry_processing.utils.custom_datagen import GroupedDatagen
 
 
-NUM_BATCHES = 20
-BATCH_SIZE = 64
+NUM_BATCHES = 150
 
 
 def entropy(x):
@@ -35,99 +34,35 @@ def fc2_normal_entropy(x, y, fc2_layer, fc2_normalize, softmax_layer):
     return x_fc2_normal, x_entropy, y
 
 
-def top_one_svm(fc2, train_datagen, valid_datagen, plot=True):
-    # Normalize the activation feature vectors.
-    fc2_normalize = samplewise_normalize(FC2_MEAN, FC2_STD)
-
-    # SGDClassifier with hinge loss and l2 is an SVM.
-    svm = linear_model.SGDClassifier(penalty='l2', alpha=0.0001, loss='hinge')
-
-    # Used for logging training details.
-    valid_score = list()
-    valid_ticks = list()
-
-    train_score = list()
-    train_ticks = list()
-
-    if plot:
-        plt.ion()
-        plt.xlabel('Batch')
-        plt.ylabel('Accuracy')
-
-        train_plot = plt.plot(train_ticks, train_score, label='train score')[0]
-        valid_plot = plt.plot(valid_ticks, valid_score, label='valid score')[0]
-        plt.legend(loc='best')
-
-    for i, batch in enumerate(zip(train_datagen, valid_datagen)):
-        if i >= NUM_BATCHES:
-            break
-        print('Batch #%d - %.2f%% completed.' % (i, i * 100.0 / NUM_BATCHES))
-
-        train_batch, valid_batch = batch
-
-        # Prep data for training.
-        x, y = train_batch
-        x = fc2.predict(x)
-        x = np.apply_along_axis(fc2_normalize, 1, x)
-        y = np.argmax(y, axis=1)
-
-        x_valid, y_valid = valid_batch
-        x_valid = fc2.predict(x_valid)
-        x_valid = np.apply_along_axis(fc2_normalize, 1, x_valid)
-        y_valid = np.argmax(y_valid, axis=1)
-
-        # Train on batch.
-        svm.partial_fit(x, y, classes=range(NUM_CLASSES))
-
-        # Add accuracy to array.
-        train_ticks.append(i)
-        train_score.append(svm.score(x, y))
-        valid_ticks.append(i)
-        valid_score.append(svm.score(x_valid, y_valid))
-
-        print("Training accuracy %.3f" % train_score[-1])
-        print("Validation accuracy %.3f" % valid_score[-1])
-
-        # Update plot live.
-        if plot:
-            valid_plot.set_data(valid_ticks, valid_score)
-            train_plot.set_data(train_ticks, train_score)
-
-            plt.axes().relim()
-            plt.axes().autoscale_view(True, True, True)
-
-            plt.pause(0.05)
-
-    # Save trained svm.
-    svm_pickle = "svm_%s.pkl" % time.strftime("%m_%d_%H_%M")
-    with open(svm_pickle, 'wb') as fid:
-        pickle.dump(svm, fid)
-    print("Saved to %s." % svm_pickle)
-
-
 def evaluate_using_k(fc2_layer, softmax_layer, train_group, valid_group,
-        top_k=5, batch_size=8):
+        top_k=3, batch_size=32, log_file=None):
     # Normalize the activation feature vectors.
     fc2_normalize = samplewise_normalize(FC2_MEAN, FC2_STD)
 
     # SGDClassifier with hinge loss and l2 is an SVM.
-    svm = linear_model.SGDClassifier(penalty='l2', alpha=0.0001, loss='hinge')
+    svm = linear_model.SGDClassifier(penalty='l2', alpha=0.0002, loss='hinge')
+
+    print("Top k: %d" % top_k)
+
+    if log_file is not None:
+        log_file.write("batch,train_acc,val_acc\n")
+        log_file.flush()
 
     for t, batch in enumerate(zip(train_group.generate(batch_size=batch_size),
                                   valid_group.generate(batch_size=batch_size))):
         if t >= NUM_BATCHES:
             break
-        print('Batch #%d - %.2f%% completed.' % (t, t * 100.0 / NUM_BATCHES))
+        print("Batch %d/%d" % (t+1, NUM_BATCHES))
 
         # Images are mean centered and normalized.
         train, valid = batch
 
         # TODO: don't hardcode these.
         examples = np.zeros((batch_size, 2048))
-        labels = np.zeros((batch_size, 10))
+        labels = np.zeros((batch_size))
 
-        valid_examples = np.zeros((batch_size, 2048))
-        valid_labels = np.zeros((batch_size, 10))
+        examples_valid = np.zeros((batch_size, 2048))
+        labels_valid = np.zeros((batch_size))
 
         for i in range(batch_size):
             # Prep data for training.
@@ -137,30 +72,41 @@ def evaluate_using_k(fc2_layer, softmax_layer, train_group, valid_group,
             x_fc_valid, x_sm_valid, y_valid = fc2_normal_entropy(valid[0][i],
                     valid[1][i], fc2_layer, fc2_normalize, softmax_layer)
 
-            print("Sorting.")
+            # Sort by entropy.
             x_sm.sort(key=lambda entropy_index: entropy_index[0])
             x_sm_valid.sort(key=lambda entropy_index: entropy_index[0])
 
-            print("Element wise max")
-            for k in range(top_k):
-                for j in range(2048):
-                    k_index = x_sm[k][1]
+            # Create a chunk using only the most confident views.
+            top_k_fc = np.zeros((top_k, x_fc.shape[1]))
+            top_k_fc_valid = np.zeros((top_k, x_fc_valid.shape[1]))
 
-                    examples[i][j] = max(examples[i][j], x_fc[k_index][j])
-                    valid_examples[i][j] = max(valid_examples[i][j],
-                            x_fc_valid[k_index][j])
+            # Grab the top k.
+            for j in range(top_k):
+                top_k_fc[j] = x_fc[x_sm[j][1]]
+                top_k_fc_valid[j] = x_fc_valid[x_sm_valid[j][1]]
 
-            print(y[0], y[1])
+            # Form the activation vector, which is element wise max of top k.
+            examples[i] = np.max(top_k_fc, axis=0)
             labels[i] = y[0]
-            valid_labels[i] = y_valid[0]
+
+            examples_valid[i] = np.max(top_k_fc_valid, axis=0)
+            labels_valid[i] = y_valid[0]
 
         # Train on batch.
         svm.partial_fit(examples, labels, classes=range(NUM_CLASSES))
 
-        # Add accuracy to array.
-        print("Train: %.4f" % svm.score(examples, labels))
-        print("Valid: %.4f" % svm.score(valid_examples, valid_labels))
+        if log_file is not None:
+            log_file.write("%d,%.4f,%.4f\n" %
+                    (t, svm.score(examples, labels),
+                        svm.score(examples_valid, labels_valid)))
+            log_file.flush()
 
+        if t > 0 and t % 25 == 0:
+            # Save trained svm.
+            svm_pickle = "svm_top_k_%d.pkl" % top_k
+            with open(svm_pickle, 'wb') as fid:
+                pickle.dump(svm, fid)
+            print("Saved to %s." % svm_pickle)
 
 
 if __name__ == '__main__':
@@ -175,12 +121,7 @@ if __name__ == '__main__':
     softmax_layer = extract_layer(model, 'predictions')
 
     # Sort by top K minimized entropy.
-    evaluate_using_k(fc2_layer, softmax_layer, train_group,
-            valid_group, 3)
-
-    if False:
-        # Initialize data generators.
-        train_datagen = get_data(TRAIN_DIR, BATCH_SIZE, preprocess=img_normalize)
-        valid_datagen = get_data(VALID_DIR, BATCH_SIZE, preprocess=img_normalize)
-
-        top_one_svm(fc2_layer, train_datagen, valid_datagen)
+    for k in [1, 3, 5, 7]:
+        with open("top_%d.log" % k, "w") as log_file:
+            evaluate_using_k(fc2_layer, softmax_layer,
+                    train_group, valid_group, top_k=k, log_file=log_file)
